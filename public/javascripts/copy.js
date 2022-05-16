0e06295b25eded84cb4758aefae0df812e589817
singingw <op0408io1998@gmail.com>
2022-05-16 12:29:51 +0800

    重構程式碼

--- public/javascripts/copy.js
+++ public/javascripts/copy.js
@@ -1,4 +1,4 @@
-module.exports = (copyEvent) => {
+function copyEvent() {
   const URL = document.getElementById("copyText")
   navigator.clipboard.writeText(URL.innerText)
     .then(() => alert('已複製連結！'))
@@ -6,3 +6,4 @@ module.exports = (copyEvent) => {
 }
 //Clipboard 指的是瀏覽器中的系統剪貼簿
 //要取得瀏覽器的 Clipboard 物件，只要訪問 navigator 底下的 clipboard 屬性
+module.exports = copyEvent
