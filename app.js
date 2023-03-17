e32338b6ab866dbc38ff8bdd863a212278108d4e
singingw <op0408io1998@gmail.com>
2023-03-17 22:19:27 +0800

    佈署至 fly.io

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const routes = require('./routes')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 require("./config/mongoose")
 
 app.use(express.urlencoded({ extended: true }))
